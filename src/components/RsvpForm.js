a5e95822cdc8279746f055470b3d30fbfbd34f38
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2017-06-07 22:32:26 -0700

    react-bootstrap based visual form

--- src/components/RsvpForm.js
+++ src/components/RsvpForm.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import request from 'request';
+import {
+  Button,
+  Checkbox,
+  ControlLabel,
+  FormControl,
+  FormGroup,
+  HelpBlock,
+  Radio
+} from 'react-bootstrap';
+
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
 
 class RsvpForm extends React.Component {
   constructor(props) {
@@ -36,60 +55,97 @@ class RsvpForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <label>
-          Name(s):
-          <input
+      <div>
+        <form>
+          <FieldGroup
+            id="formControlsNames"
             type="text"
-            value={this.state['names']}
-            onChange={this.handleChange.bind(this, 'names')}
+            label="Name(s)"
+            placeholder="Enter your (and your guest's) names"
           />
-        </label>
-        <br />
+          <FieldGroup
+            id="formControlsEmail"
+            type="email"
+            label="Email address"
+            placeholder="Enter a contact email address"
+          />
 
-        <label>
-          <input
-            type="radio"
-            name="isAttending"
-            value={true}
-            checked={this.state.isAttending === "true"}
-            onChange={this.handleChange.bind(this, 'isAttending')}
-          />
-            Accepts with hysterical pleasure
-        </label>
-        <br />
+          <FormGroup>
+            <Radio name="radioGroup">
+              Accepts with hysterical pleasure
+            </Radio>
+            <Radio name="radioGroup">
+              Declines with extreme regret
+            </Radio>
+          </FormGroup>
 
-        <label>
-          <input
-            type="radio"
-            name="isAttending"
-            value={false}
-            checked={this.state.isAttending === "false"}
-            onChange={this.handleChange.bind(this, 'isAttending')}
-          />
-            Declines with extreme regret
-        </label>
-        <br />
+          <FormGroup controlId="formControlsTextarea">
+            <ControlLabel>Comments</ControlLabel>
+            <FormControl componentClass="textarea" placeholder="Enter additional comments" />
+          </FormGroup>
 
-        <label>
-          Dietary restrictions:
-          <input
-            type="text"
-            value={this.state['dietRestrict']}
-            onChange={this.handleChange.bind(this, 'dietRestricts')}
-          />
-        </label>
-        <br />
-        <label>
-          Additional Comments:
-          <textarea
-            value={this.state['comments']}
-            onChange={this.handleChange.bind(this, 'comments')}
-          />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+          <Button type="submit">
+            Submit
+          </Button>
+        </form>
+
+        <hr/>
+
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label>
+            Name(s):
+            <input
+              type="text"
+              value={this.state['names']}
+              onChange={this.handleChange.bind(this, 'names')}
+            />
+          </label>
+          <br />
+
+          <label>
+            <input
+              type="radio"
+              name="isAttending"
+              value={true}
+              checked={this.state.isAttending === "true"}
+              onChange={this.handleChange.bind(this, 'isAttending')}
+            />
+              Accepts with hysterical pleasure
+          </label>
+          <br />
+
+          <label>
+            <input
+              type="radio"
+              name="isAttending"
+              value={false}
+              checked={this.state.isAttending === "false"}
+              onChange={this.handleChange.bind(this, 'isAttending')}
+            />
+              Declines with extreme regret
+          </label>
+          <br />
+
+          <label>
+            Dietary restrictions:
+            <input
+              type="text"
+              value={this.state['dietRestrict']}
+              onChange={this.handleChange.bind(this, 'dietRestricts')}
+            />
+          </label>
+          <br />
+          <label>
+            Additional Comments:
+            <textarea
+              value={this.state['comments']}
+              onChange={this.handleChange.bind(this, 'comments')}
+            />
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
